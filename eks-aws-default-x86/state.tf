5e3ecf538fe15e1310a81ced2237fe102cbb1c1c
manupanand <manupanandindianinvestor@gmail.com>
2025-11-08 23:51:30 +0530

    updeate config

--- eks-aws-default-x86/state.tf
+++ eks-aws-default-x86/state.tf
@@ -2,4 +2,14 @@ terraform {
   backend "s3" {
     
   }
+  required_providers {
+      helm = {
+        source  = "hashicorp/helm"
+        version = "~> 2.11"
+      }
+      kubernetes = {
+        source  = "hashicorp/kubernetes"
+        version = "~> 2.29"
+      }
+    }
 }
